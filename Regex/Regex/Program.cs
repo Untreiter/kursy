264f1887ccf7414a0990ead2c21bb5a0bcd33a19
Untreiter <45892307+Untreiter@users.noreply.github.com>
2019-12-18 10:15:00 +0300

    Task2

---
+++ Regex/Regex/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegexT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s;
+            Console.WriteLine("Enter any string:");
+            s = Console.ReadLine();
+
+            Console.WriteLine("Count of vowels: {0}",reg(s));
+            Console.ReadKey();
+        }
+
+        static int reg (string s)
+        {
+            Regex r = new Regex("[aeiou]");
+            MatchCollection match = r.Matches(s);
+            return match.Count;
+        }
+    }
+}
